0de4812c86e565b4ae6cf765a822d68aaa42988a
Marcus <64868214+Marcus1200@users.noreply.github.com>
2020-08-08 15:57:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ senha1 = inputsenha
     if(senha1.value == senha) {
      nome1.innerHTML= "Bem vindo: "
      alert("Seu nome de usuario é: "+ nome.value)
-     document.write(`<h1>Bem vindo ao bate papo online</h1><h2>${nome.value}</h2>`)
+     document.write(`<h1>codigo: 01100110 01100101 01101100 01101001 01111010 00100000 01100100 01101001 01100001 00100000 01100100 01101111 01110011 00100000 01110000 01100001 01101001 01110011 </h1><h2>${nome.value}</h2>`)
 
   }
  if(senha1.value != senha){
